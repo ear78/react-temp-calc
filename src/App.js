1b6b6a799c228cf0a1cf5e3e39268f806bc45f3a
Elliot Richardson <elliotrichardson@ITL01WS-ENG-ER1.local>
2017-12-05 17:14:34 -0600

    added conversion functions and temp inputs for both scales.

--- src/App.js
+++ src/App.js
@@ -10,12 +10,96 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Calculator />
       </div>
     );
   }
 }
 
 export default App;
+
+function BoilingVerdict(props){
+    if(props.celsius >= 100){
+        return <p>The water would boil.</p>
+    }
+    return <p>The water would not boil.</p>
+}
+
+class Calculator extends React.Component{
+    constructor(props){
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {temperature: ''};
+    }
+
+    handleChange(e){
+        this.setState({temperature: e.target.value})
+    }
+    render(){
+        const temperature = this.state.temperature;
+        return (
+            <fieldset>
+                <legend>Enter temperature in Celsius:</legend>
+                <input
+                    value={temperature}
+                    onChange={this.handleChange} />
+
+                <BoilingVerdict
+                    celsius={parseFloat(temperature)} />
+
+                <TemperatureInput scale="c" />
+                <TemperatureInput scale="f" />
+            </fieldset>
+
+        )
+    }
+}
+
+// SCALE CONVERSION
+function toCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius) {
+  return (celsius * 9 / 5) + 32;
+}
+
+// NUMBER CONVERSION
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return '';
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+
+// SCALE NAMES
+const scaleNames = {
+    c: 'Celsius',
+    f: 'Fahrenheit'
+};
+
+class TemperatureInput extends React.Component{
+    constructor(props){
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e){
+        this.setState({temperature: e.target.value});
+    }
+
+    render(){
+        const temperature = this.props.temperature;
+        const scale = this.props.scale;
+        return (
+            <fieldset>
+                <legend>Enter temperature in {scaleNames[scale]}:</legend>
+                <input value={temperature}
+                       onChange={this.handleChange} />
+            </fieldset>
+        )
+    }
+}
